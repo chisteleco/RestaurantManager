cc2c57bf34903298c17858ba4aa84b9a35365fe9
Dani <chisteleco@gmail.com>
2022-08-25 18:28:45 +0200

    first custom view with some pages

---
+++ restaurant-manager/pages/supplies/index.js
@@ -0,0 +1,11 @@
+import Head from 'next/head'
+import Image from 'next/image'
+
+
+export default function Home() {
+  return (
+    <>
+        <h1>TO DO. This should show a supplies data page</h1>
+    </>
+  )
+}
